3e3b03d62274f79cb609d19bd8734640bc8bdeed
Zebas-X <rodrigorojitoz@gmail.com>
2024-09-08 20:51:27 -0400

    Laboratorio + desafio

---
+++ Laboratorio/Contacts.java
@@ -0,0 +1,53 @@
+import java.io.Serializable;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String email;
+    private Integer age;
+    private String phone;
+
+    public Contact(String name, String email, int age, String phone) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Email: " + email + ", Age: " + age + ", Phone: " + phone;
+    }
+}
